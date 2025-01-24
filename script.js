1966b075dd55092b63ead28306f78d25bc47ad6f
Shivam Tiwari <synoep@gmail.com>
2025-01-24 17:11:13 +0530

    Normalize line endings

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-// const fs = require('fs');
+ // Correctly using the response object
 
 // fs.writeFile("hey.txt",'hey hello kaise ho', function(err){
 //     if(err) console.error(err);
@@ -36,13 +35,71 @@
 
 // server.listen(3000);
 
+
+const morgan = require('morgan')
+
 const express  = require('express');
 const  app = express()
 
-app.set("view engine",'ejs')
+const userModel = require('./models/user')
+const dbConnection = require('./config/db')
 
-app.get('/about',(req,res) => {
-     res.render('index')           
-})
+app.use(morgan('dev'))
 
-app.listen(3000)
+app.set("view engine",'ejs')  
+
+app.use(express.json()) ;// these two lines are built in middle ware use for getting data in req.body
+app.use(express.urlencoded({extended:true}));// for geeting data in post method in exp  
+
+app.use(express.static("public")) // middlewares for connecting CSS
+
+
+
+
+
+
+
+
+
+ // html page rendering from views folder
+
+
+// app.use((req,res,next)=>{
+//      console.log("This is middleware")
+//      // 3 types middle warews 
+//      //1.built in 
+//      //2. coustom middlewares
+//      //3. third party middlewares
+//      return next() // chalo rakho  
+// })
+
+// app.get('/about',(req,res) => {
+//      res.render('index')           
+// })
+
+// app.get('/about',  (req,res,next) => {
+//       const a=5;
+//       const b=10;
+//       console.log(a+b)
+//       next()
+// },
+//      (req,res) => {    // using middleware in rotes
+//      res.render('index')           
+// }) 
+
+// app.listen(3000)
+
+
+// form controller 
+
+app.get('/about', (req,res) => {   // get - server - frontend
+         res.render('index')    ;     
+ }) 
+
+ app.post('/get-form-data', (req,res) => {   // post- frontend to backend server
+    console.log(req.body);
+    res.send('data received');         
+}) ;
+
+
+ app.listen(3000)
